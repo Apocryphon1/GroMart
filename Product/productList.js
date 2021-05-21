26069bd17ed7d59e23d674864b975f7c716615ea
Apocryphon1 <46965861+Apocryphon1@users.noreply.github.com>
2021-05-21 17:57:49 +0200

    Merge Elwi work

---
+++ Product/productList.js
@@ -0,0 +1,62 @@
+
+$(document).ready(function () {
+    var submitIcon = $('.searchbox-icon');
+    var inputBox = $('.searchbox-input');
+    var searchBox = $('.searchbox');
+    var isOpen = false;
+    submitIcon.click(function () {
+        if (isOpen == false) {
+            searchBox.addClass('searchbox-open');
+            inputBox.focus();
+            isOpen = true;
+        } else {
+            searchBox.removeClass('searchbox-open');
+            inputBox.focusout();
+            isOpen = false;
+        }
+    });
+    submitIcon.mouseup(function () {
+        return false;
+    });
+    searchBox.mouseup(function () {
+        return false;
+    });
+    $(document).mouseup(function () {
+        if (isOpen == true) {
+            $('.searchbox-icon').css('display', 'block');
+            submitIcon.click();
+        }
+    });
+});
+function buttonUp() {
+    var inputVal = $('.searchbox-input').val();
+    inputVal = $.trim(inputVal).length;
+    if (inputVal !== 0) {
+        $('.searchbox-icon').css('display', 'none');
+    } else {
+        $('.searchbox-input').val('');
+        $('.searchbox-icon').css('display', 'block');
+    }
+}
+
+
+$(document).ready(function () {
+    $("#flip").click(function () {
+        $("#panel").slideDown("slow");
+    });
+});
+
+
+
+function indexCart(name, price) {
+    var item = new Item(name, price);
+
+    document.getElementById("shc").innerHTML += "<br>  " + item.name;
+
+
+}
+
+function Item(name, price) {
+    this.name = name;
+    this.price = price;
+}
