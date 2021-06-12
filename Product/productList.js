e1dc52b57381a77caa20e2271bffc212a26b8344
elwifayed <alifayed2001@hotmail.com>
2021-06-12 19:52:22 +0200

    add product list

--- Product/productList.js
+++ Product/productList.js
@@ -27,6 +27,12 @@ $(document).ready(function () {
             submitIcon.click();
         }
     });
+    $(document).on("keypress", "input", function(e){
+        if(e.which == 13){
+            var inputVal = $(this).val();
+            //if (inputVal == %pepsi%)
+        }
+    });
 });
 function buttonUp() {
     var inputVal = $('.searchbox-input').val();
@@ -45,18 +51,55 @@ $(document).ready(function () {
         $("#panel").slideDown("slow");
     });
 });
-
-
-
-function indexCart(name, price) {
-    var item = new Item(name, price);
-
+function indexCart(id, name, price, quantity, manufacture ,image) {
+    var item = new Item(id, name, price, quantity, manufacture ,image);
+    //document.cookie('id= '+item.id+' name= '+ item.name+' price= '+item.price+' quantity= '+item.quantity+' manufacture= ' + item.manufacture+' image= '+item.image);
     document.getElementById("shc").innerHTML += "<br>  " + item.name;
-
-
 }
 
-function Item(name, price) {
+function Item(id,name,price,quantity,manufacture,image) {
+    this.id = id;
     this.name = name;
     this.price = price;
+    this.quantity = quantity;
+    this.manufacture = manufacture;
+    this.image = image;
+}
+
+
+allUsers = [];
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i <ca.length; i++) {
+      var c = ca[i];
+      while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+    return "";
+  }
+
+function printCookie(){
+    var list = getCookie(Products);
+    document.write(list);
+}
+var modal = document.getElementById("modalP");
+var btn = document.getElementById("ModalBtn");
+var span = document.getElementsByClassName("closeProd")[0];
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+span.onclick = function() {
+  modal.style.display = "none";
+}
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
 }
